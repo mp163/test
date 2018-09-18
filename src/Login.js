e218e6471039385453cb74876dc97d46d539918a
MP163 <mp163@yandex.ru>
2018-09-18 21:31:32 +0400

    add error login

--- src/Login.js
+++ src/Login.js
@@ -12,12 +12,16 @@ class Login extends React.Component{
     logIn(){
         let enterUser=document.getElementById('log').value;
         let enterPassword=document.getElementById('pass').value;
+        let uncorrectPassword=true;
         users.map((user)=>{
             if ((user.login===enterUser)&&(user.pass===enterPassword)){
                 this.props.onLogIn(user.uid);
                 this.props.history.push('/list');
+                uncorrectPassword=false;
             }
         });
+        if (uncorrectPassword) document.getElementsByTagName("span")[0].className="errorLogin";
+      // console.log(document.getElementsByTagName("span")[0]);
        
     }
 
@@ -27,6 +31,7 @@ class Login extends React.Component{
                 <h1>Авторизуйтесь</h1>
                 <label>Логин: </label><input type="text" id="log" /><br/>
                 <label>Пароль: </label><input type="password" id="pass" /><br/>
+                <span className="errorSpan">Неверный логин или пароль</span>
                 <button onClick={this.logIn}>Войти</button>
             </div>
         )
